c79baffa0fc095cf19964e43b3e25991d1447d72
Lewis Renfrew <LewisRe@linn.co.uk>
2019-01-15 12:09:12 +0000

    deleting unused stuff

--- src/components/ReportTable.js
+++ src/components/ReportTable.js
@@ -1,67 +1,149 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { withStyles } from "@material-ui/core/styles";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
-import Collapse from "@material-ui/core/Collapse";
-import ExpandLess from "@material-ui/icons/ExpandLess";
-import ExpandMore from "@material-ui/icons/ExpandMore";
+import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import classnames from 'classnames';
+import { formatTitle, displayError, setDrilldown, setValueDrilldown, setTextValueDrilldown } from '../utilities/DisplayUtilities';
+import { reportResultType } from '../propTypes/index'
+import PropTypes from 'prop-types';
+import { createMuiTheme } from '@material-ui/core/styles';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 
 const styles = theme => ({
   root: {
-    width: "100%",
+    width: '100%',
+    margin: theme.spacing.unit * 3,
+
+    overflowX: 'auto',
   },
-  nested: {
-    paddingLeft: theme.spacing.unit * 4,
+  table: {
+
+    margin: theme.spacing.unit * 3,
   },
-  titles: {
-    paddingLeft: theme.spacing.unit * 4,
-    fontWeight: "bold !important",
-  }
 });
 
-class MenuList extends React.Component {
+const theme = createMuiTheme({
+    overrides: {
+      MuiTableCell: {
+        body: {
+          fontSize:"12px"
+        },
+        head: {
+          fontSize:"14px"
+        }
+      }
+    }
+});
 
-  state = {
-    open: false
-  };
+const getCellClassName = (isVarianceColumn, isTotalColumn, value, textValue, defaults = []) => classnames(
+    defaults,
+    { 'variance-positive': (isVarianceColumn && value > 0) },
+    { 'variance-negative': (isVarianceColumn && value < 0) },
+    { 'success total': (isTotalColumn) },
+    { 'text-right': (!textValue) }
+);
 
-  handleClick = () => {
-    this.setState(state => ({ open: !state.open }));
-  };
+const getTableClassNames = (containsSubtotals, defaults = []) => classnames(
+    defaults, 'table', 'table-condensed', 'table-bordered', 'small',
+    { 'table-striped': !containsSubtotals }
+);
 
-  render() {
-    const { classes, title, entries } = this.props;
-    return (
-      <List
-        component="nav"
-        className={classes.root}
-      >
+const getTableRowClassNames = (rowType, containsSubtotals, defaults = []) => classnames(
+    defaults,
+    { 'subtotal-row active': containsSubtotals && rowType === 'Subtotal' },
+    { 'success': rowType === 'Total' }
+);
 
-        <ListItem button onClick={this.handleClick} >
-          <ListItemText> {title}</ListItemText>
-          {this.state.open ? <ExpandLess /> : <ExpandMore />}
-        </ListItem>
+const getTableHeaderClassNames = (cellHeader, fixColumnWidth, isTextColumn, defaults = []) => classnames(
+    defaults,
+    { 'col-xs-1': fixColumnWidth && cellHeader },
+    { 'col-xs-2': fixColumnWidth && !cellHeader },
+    { 'text-right': !(isTextColumn) }
+);
 
-        <Collapse in={this.state.open} timeout="auto" unmountOnExit>
-          <List component="div" disablePadding>
-            {entries.map((entry, index) => (
-              entry.showInMenu ? (
-                <a key={entry.title} href={ entry.href}>
-                  <ListItem key={entry.title + index} button className={classes.nested}  >
-                    <ListItemText  primary={entry.title} />
-                  </ListItem> </a>) : <span key={entry.title}/>
-            ))}
-          </List>
-        </Collapse>
-      </List>
+const Placeholder = ({ rows, columns }) => (
+    <Table className="table placeholder table-placeholder">
+        <TableBody>
+            {
+                [...Array(rows).keys()].map((row) => (
+                    <TableRow key={row}>
+                        {[...Array(columns).keys()].map((column) => (
+                            <TableCell key={column}></TableCell>
+                        ))}
+                    </TableRow>
+                ))
+            }
+        </TableBody>
+    </Table>);
+
+const ReportTable = (
+    {
+        reportData,
+        title,
+        showTitle = true,
+        showTotals = true,
+        placeholderRows = 5,
+        placeholderColumns = 6,
+        containsSubtotals = false,
+        fixColumnWidths = false,
+        showRowTitles = true
+    }) => (
+        <MuiThemeProvider theme={theme}>
+        <Paper className={styles.root}>
+            {formatTitle(title, showTitle, !reportData, reportData && reportData.error, reportData ? reportData.reportHelpText : null)}
+            {!reportData
+                ? <Placeholder rows={placeholderRows} columns={placeholderColumns} /> :
+                reportData.error ? displayError(reportData.message)
+                    : <div style={{backgroundColor: "white"}}>
+                        <Table className={getTableClassNames(containsSubtotals)}>
+                                <TableHead key="headers">
+                                <TableRow>
+                                    {showRowTitles ? (<TableCell className={getTableHeaderClassNames(false, fixColumnWidths, '')}></TableCell>) : null}
+                                    {reportData.headers.columnHeaders
+                                        .map((header, i) => (<TableCell className={getTableHeaderClassNames(true, fixColumnWidths, reportData.headers.textColumns.includes(i), [])} key={i}>{header}</TableCell>))}
+                                </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                {reportData.results.map((item, j) => (
+                                    <TableRow className={getTableRowClassNames(item.rowType, containsSubtotals, ['report-data-row'])} key={j} >
+                                        {showRowTitles ? (<TableCell className="single-line-field" data-tip={item.rowTitle.displayString}>{setDrilldown(item.rowTitle)}</TableCell>) : null}
+                                        {item.values
+                                            .map((value, i) => (<TableCell className={getCellClassName(reportData.headers.varianceColumns.includes(i) || reportData.headers.varianceRows.includes(j), reportData.headers.totalColumns.includes(i), value.displayValue, value.textDisplayValue, [])} key={i}>{setValueDrilldown(value)}{setTextValueDrilldown(value)}</TableCell>))}
+                                    </TableRow>
+                                ))}
+
+                                {showTotals
+                                    ? (<TableRow className={getTableRowClassNames(reportData.totals.rowType, containsSubtotals, ['report-totals'])} key="totals">
+                                        {showRowTitles ? (<TableCell>{reportData.totals.rowTitle.displayString}</TableCell>) : null}
+                                        {reportData.totals.values
+                                            .map((value, i) => (<TableCell className={getCellClassName(reportData.headers.varianceColumns.includes(i), reportData.headers.totalColumns.includes(i), value.displayValue, value.textDisplayValue, ['total'])} key={i}>{setValueDrilldown(value)}</TableCell>))}
+                                    </TableRow>)
+                                    : false
+                                }
+                            </TableBody>
+                        </Table>
+                    </div>
+            }
+        </Paper>
+        </MuiThemeProvider>
     );
-  }
+
+Table.propTypes = {
+    reportData: reportResultType,
+    title: PropTypes.object,
+    showTitle: PropTypes.bool,
+    showTotals: PropTypes.bool,
+    placeholderRows: PropTypes.number,
+    placeholderColumns: PropTypes.number,
+    containsSubtotals: PropTypes.bool,
+    fixColumnWidths: PropTypes.bool,
+    showRowTitles: PropTypes.bool
 }
 
-MenuList.propTypes = {
-  classes: PropTypes.object.isRequired
-};
 
-export default withStyles(styles)(MenuList);
+export default withStyles(styles)(ReportTable);
--- src/stories/Navigation.stories.js
+++ src/stories/Navigation.stories.js
@@ -6,9 +6,6 @@ import  Navbar  from '../components/Navbar';
 import { withKnobs, object } from '@storybook/addon-knobs/react';
 import { boolean, text } from '@storybook/addon-knobs';
 
-
-  
-
 const stories = storiesOf('Navigation', module);
 stories.addDecorator(StoryRouter());
 
