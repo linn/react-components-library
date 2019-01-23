d34f6288990deb445e4f69f3137227c49d0f9a14
Adam Campbell <Adam.Campbell@linn.co.uk>
2019-01-23 16:36:58 +0000

    Add input field

---
+++ src/components/InputField.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { TextField, InputAdornment } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = () => ({});
+
+function InputField(props) {
+    const {
+        adornment,
+        classes,
+        disabled,
+        error,
+        fullWidth,
+        helperText,
+        label,
+        margin,
+        multiline,
+        name,
+        placeholder,
+        type,
+        value,
+        onChange
+    } = props;
+
+    return (
+        <TextField
+            className={classes.root}
+            disabled={disabled}
+            error={error}
+            fullWidth={fullWidth}
+            helperText={helperText}
+            label={label}
+            margin={margin}
+            multiline={multiline}
+            name={name}
+            placeholder={placeholder}
+            rows={multiline ? 4 : ''}
+            type={type}
+            value={value}
+            onChange={onChange}
+            InputLabelProps={{ shrink: true }}
+            InputProps={
+                adornment
+                    ? {
+                          startAdornment: (
+                              <InputAdornment position="start">{adornment}</InputAdornment>
+                          )
+                      }
+                    : {}
+            }
+            variant="outlined"
+        />
+    );
+}
+
+InputField.propTypes = {
+    adornment: PropTypes.string,
+    classes: PropTypes.shape({}).isRequired,
+    disabled: PropTypes.bool,
+    error: PropTypes.bool,
+    fullWidth: PropTypes.bool,
+    helperText: PropTypes.string,
+    label: PropTypes.string.isRequired,
+    margin: PropTypes.string,
+    multiline: PropTypes.bool,
+    name: PropTypes.string.isRequired,
+    placeholder: PropTypes.string,
+    type: PropTypes.string,
+    value: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired
+};
+
+InputField.defaultProps = {
+    adornment: '',
+    disabled: false,
+    error: false,
+    fullWidth: false,
+    helperText: '',
+    margin: 'normal',
+    multiline: false,
+    placeholder: '',
+    type: 'text'
+};
+
+export default withStyles(styles)(InputField);
---
+++ src/stories/InputField.stories.js
@@ -0,0 +1,179 @@
+import React, { Fragment } from "react";
+import { storiesOf } from "@storybook/react";
+import { withKnobs } from "@storybook/addon-knobs/react";
+import { text } from "@storybook/addon-knobs";
+import { action } from "@storybook/addon-actions";
+import { Grid } from '@material-ui/core';
+import InputField from '../components/InputField';
+
+const actions = {
+    onChange: action('onChange')
+}
+
+storiesOf('InputField', module)
+    .addDecorator(story => <div>{story()}</div>)
+    .addDecorator(withKnobs)
+    .add('Label and value', () => (
+        <InputField
+            value={text('value', 'Value')}
+            label={text('label', 'Input Field')}
+            name={text('name', 'storybook')}
+            {...actions}
+        />
+    ))
+    .add('Label no value', () => (
+        <InputField
+            value={text('value', '')}
+            label={text('label', 'No Value')}
+            name={text('name', 'storybook')}
+            {...actions}
+        />
+    ))
+    .add('Full width', () => (
+        <InputField
+            value={text('value', 'Full Width')}
+            label={text('label', 'Full Width')}
+            name={text('name', 'storybook')}
+            fullWidth={true}
+            {...actions}
+        />
+    ))
+    .add('Error', () => (
+        <InputField
+            value={text('value', 'Error')}
+            label={text('label', 'Error')}
+            name={text('name', 'storybook')}
+            error={true}
+            {...actions}
+        />
+    ))
+    .add('Disabled', () => (
+        <InputField
+            value={text('value', 'Disabled')}
+            label={text('label', 'Disabled')}
+            name={text('name', 'storybook')}
+            disabled={true}
+            {...actions}
+        />
+    ))
+    .add('Date', () => (
+        <InputField
+            value={text('value', '')}
+            label={text('label', 'Date')}
+            name={text('name', 'storybook')}
+            type='date'
+            {...actions}
+        />
+    ))
+    .add('Number', () => (
+        <InputField
+            value={text('value', '123.45')}
+            label={text('label', 'Number')}
+            name={text('name', 'storybook')}
+            type='number'
+            {...actions}
+        />
+    ))
+    .add('With adornment', () => (
+        <InputField
+            value={text('value', 'With adornment')}
+            label={text('label', 'With adornment')}
+            name={text('name', 'storybook')}
+            adornment='$'
+            {...actions}
+        />
+    ))
+    .add('Multiline', () => (
+        <InputField
+            value={text('value', 'Multiline')}
+            label={text('label', 'Multiline')}
+            name={text('name', 'storybook')}
+            multiline={true}
+            {...actions}
+        />
+    ))
+    .add('Regular spacing', () => (
+        <Fragment>
+            <div style={{ width: '50%' }}>
+                <Grid container>
+                    <Grid item xs={6}>
+                        <InputField
+                            value={text('value', 'value')}
+                            label={text('label', 'Normal Spacing')}
+                            name={text('name', 'storybook')}
+                            {...actions}
+                        />
+                    </Grid>
+                    <Grid item xs={6}>
+                        <InputField
+                            value={text('value', 'value')}
+                            label={text('label', 'Normal Spacing')}
+                            name={text('name', 'storybook')}
+                            {...actions}
+                        />
+                    </Grid>
+                    <Grid item xs={6}>
+                        <InputField
+                            value={text('value', 'value')}
+                            label={text('label', 'Normal Spacing')}
+                            name={text('name', 'storybook')}
+                            {...actions}
+                        />
+                    </Grid>
+                    <Grid item xs={6}>
+                        <InputField
+                            value={text('value', 'value')}
+                            label={text('label', 'Normal Spacing')}
+                            name={text('name', 'storybook')}
+                            {...actions}
+                        />
+                    </Grid>
+                </Grid>
+            </div>
+        </Fragment>
+    ))
+    .add('Compact spacing', () => (
+        <Fragment>
+            <div style={{ width: '50%' }}>
+                <Grid container>
+                    <Grid item xs={6}>
+                        <InputField
+                            value={text('value', 'value')}
+                            label={text('label', 'Compact Spacing')}
+                            name={text('name', 'storybook')}
+                            margin='dense'
+                            {...actions}
+                        />
+                    </Grid>
+                    <Grid item xs={6}>
+                        <InputField
+                            value={text('value', 'value')}
+                            label={text('label', 'Compact Spacing')}
+                            name={text('name', 'storybook')}
+                            margin='dense'
+                            {...actions}
+                        />
+                    </Grid>
+                    <Grid item xs={6}>
+                        <InputField
+                            value={text('value', 'value')}
+                            label={text('label', 'Compact Spacing')}
+                            name={text('name', 'storybook')}
+                            margin='dense'
+                            {...actions}
+                        />
+                    </Grid>
+                    <Grid item xs={6}>
+                        <InputField
+                            value={text('value', 'value')}
+                            label={text('label', 'Compact Spacing')}
+                            name={text('name', 'storybook')}
+                            margin='dense'
+                            {...actions}
+                        />
+                    </Grid>
+                </Grid>
+            </div>
+        </Fragment>
+    ));
+    
