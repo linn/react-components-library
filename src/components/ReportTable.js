4b896f8979b5889539e8d66c175132d201432b53
Lewis Renfrew <LewisRe@linn.co.uk>
2019-01-29 11:50:08 +0000

    made slightly more responsive

--- src/components/ReportTable.js
+++ src/components/ReportTable.js
@@ -1,13 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-import classnames from 'classnames';
 import { formatTitle, displayError, setDrilldown, setValueDrilldown, setTextValueDrilldown } from '../utilities/DisplayUtilities';
 import { reportResultType } from '../propTypes/index'
 import PropTypes from 'prop-types';
@@ -16,13 +14,10 @@ import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 
 const styles = theme => ({
   root: {
-    width: '100%',
-    margin: theme.spacing.unit * 3,
-
-    overflowX: 'auto',
+    width: "100%",  
+    minWidth: '700px',
   },
   table: {
-
     margin: theme.spacing.unit * 3,
   },
 });
@@ -31,7 +26,8 @@ const theme = createMuiTheme({
     overrides: {
       MuiTableCell: {
         body: {
-          fontSize:"12px"
+          fontSize:"12px",
+
         },
         head: {
           fontSize:"14px"
@@ -40,34 +36,8 @@ const theme = createMuiTheme({
     }
 });
 
-const getCellClassName = (isVarianceColumn, isTotalColumn, value, textValue, defaults = []) => classnames(
-    defaults,
-    { 'variance-positive': (isVarianceColumn && value > 0) },
-    { 'variance-negative': (isVarianceColumn && value < 0) },
-    { 'success total': (isTotalColumn) },
-    { 'text-right': (!textValue) }
-);
-
-const getTableClassNames = (containsSubtotals, defaults = []) => classnames(
-    defaults, 'table', 'table-condensed', 'table-bordered', 'small',
-    { 'table-striped': !containsSubtotals }
-);
-
-const getTableRowClassNames = (rowType, containsSubtotals, defaults = []) => classnames(
-    defaults,
-    { 'subtotal-row active': containsSubtotals && rowType === 'Subtotal' },
-    { 'success': rowType === 'Total' }
-);
-
-const getTableHeaderClassNames = (cellHeader, fixColumnWidth, isTextColumn, defaults = []) => classnames(
-    defaults,
-    { 'col-xs-1': fixColumnWidth && cellHeader },
-    { 'col-xs-2': fixColumnWidth && !cellHeader },
-    { 'text-right': !(isTextColumn) }
-);
-
 const Placeholder = ({ rows, columns }) => (
-    <Table className="table placeholder table-placeholder">
+    <Table >
         <TableBody>
             {
                 [...Array(rows).keys()].map((row) => (
@@ -84,6 +54,7 @@ const Placeholder = ({ rows, columns }) => (
 const ReportTable = (
     {
         reportData,
+        classes,
         title,
         showTitle = true,
         showTotals = true,
@@ -94,34 +65,43 @@ const ReportTable = (
         showRowTitles = true
     }) => (
         <MuiThemeProvider theme={theme}>
-        <Paper className={styles.root}>
+        <Paper className={classes.root}>
             {formatTitle(title, showTitle, !reportData, reportData && reportData.error, reportData ? reportData.reportHelpText : null)}
             {!reportData
                 ? <Placeholder rows={placeholderRows} columns={placeholderColumns} /> :
                 reportData.error ? displayError(reportData.message)
                     : <div style={{backgroundColor: "white"}}>
-                        <Table className={getTableClassNames(containsSubtotals)}>
+                        <Table className={styles.table}>
                                 <TableHead key="headers">
                                 <TableRow>
-                                    {showRowTitles ? (<TableCell className={getTableHeaderClassNames(false, fixColumnWidths, '')}></TableCell>) : null}
-                                    {reportData.headers.columnHeaders
-                                        .map((header, i) => (<TableCell className={getTableHeaderClassNames(true, fixColumnWidths, reportData.headers.textColumns.includes(i), [])} key={i}>{header}</TableCell>))}
+                                    {showRowTitles ? (
+                                        <TableCell className={getTableHeaderClassNames(false, fixColumnWidths, '')}/>) : null}
+                                    {reportData.headers.columnHeaders.map((header, i) => (
+                                        <TableCell key={i}>
+                                                {header}
+                                        </TableCell>))}
                                 </TableRow>
                                 </TableHead>
                                 <TableBody>
                                 {reportData.results.map((item, j) => (
-                                    <TableRow className={getTableRowClassNames(item.rowType, containsSubtotals, ['report-data-row'])} key={j} >
+                                    <TableRow key={j} >
                                         {showRowTitles ? (<TableCell className="single-line-field" data-tip={item.rowTitle.displayString}>{setDrilldown(item.rowTitle)}</TableCell>) : null}
                                         {item.values
-                                            .map((value, i) => (<TableCell className={getCellClassName(reportData.headers.varianceColumns.includes(i) || reportData.headers.varianceRows.includes(j), reportData.headers.totalColumns.includes(i), value.displayValue, value.textDisplayValue, [])} key={i}>{setValueDrilldown(value)}{setTextValueDrilldown(value)}</TableCell>))}
+                                            .map((value, i) => (
+                                            <TableCell 
+                                                key={i}>{setValueDrilldown(value)}{setTextValueDrilldown(value)}
+                                            </TableCell>))}
                                     </TableRow>
                                 ))}
 
                                 {showTotals
-                                    ? (<TableRow className={getTableRowClassNames(reportData.totals.rowType, containsSubtotals, ['report-totals'])} key="totals">
+                                    ? (<TableRow key="totals">
                                         {showRowTitles ? (<TableCell>{reportData.totals.rowTitle.displayString}</TableCell>) : null}
                                         {reportData.totals.values
-                                            .map((value, i) => (<TableCell className={getCellClassName(reportData.headers.varianceColumns.includes(i), reportData.headers.totalColumns.includes(i), value.displayValue, value.textDisplayValue, ['total'])} key={i}>{setValueDrilldown(value)}</TableCell>))}
+                                            .map((value, i) => (
+                                            <TableCell 
+                                                key={i}>{setValueDrilldown(value)}
+                                            </TableCell>))}
                                     </TableRow>)
                                     : false
                                 }
