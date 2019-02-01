ecf5c3c41d63feb8eee091cf353856c4debb59b9
richard phillips <richard.phillips@linn.co.uk>
2019-02-01 14:55:55 +0000

    remove from index.js

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-import BackButton from "./src/components/BackButton";
-import ExportButton from "./src/components/ExportButton";
-import Breadcrumbs from "./src/components/Breadcrumbs";
-import SaveCancelButtons from "./src/components/SaveCancelButtons";
-import FormField from "./src/components/FormField";
-import ReportTable from "./src/components/ReportTable";
-import Loading from "./src/components/Loading";
-import MiniLoading from "./src/components/MiniLoading";
-import Dropdown from "./src/components/Dropdown";
-import ErrorCard from "./src/components/ErrorCard";
-import CheckboxWithLabel from "./src/components/CheckboxWithLabel";
-import Title from "./src/components/Title";
+import BackButton from './src/components/BackButton';
+import ExportButton from './src/components/ExportButton';
+import Breadcrumbs from './src/components/Breadcrumbs';
+import SaveCancelButtons from './src/components/SaveCancelButtons';
+import ReportTable from './src/components/ReportTable';
+import Loading from './src/components/Loading';
+import MiniLoading from './src/components/MiniLoading';
+import Dropdown from './src/components/Dropdown';
+import ErrorCard from './src/components/ErrorCard';
+import CheckboxWithLabel from './src/components/CheckboxWithLabel';
+import Title from './src/components/Title';
 import InputField from './src/components/InputField';
 import Typeahead from './src/components/Typeahead';
 
@@ -19,7 +18,6 @@ export {
     CheckboxWithLabel,
     Dropdown,
     ErrorCard,
-    FormField,
     SaveCancelButtons,
     ReportTable,
     ExportButton,
