90d00f8bb281608cdae8f622e2346eb2e825ab69
lewisrenfrew <Lewis.Renfrew@linn.co.uk>
2019-02-04 16:22:49 +0000

    New dropdown (#14)
    * stop linter complaining about dev dependencies
    
    * new dropdown stuff, slight change to input field

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,32 +1,87 @@
-import React from 'react';
-import { FormControl, InputLabel, Select, MenuItem } from '@material-ui/core';
-import { withStyles } from '@material-ui/core/styles';
+import { TextField } from '@material-ui/core';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-const styles = (theme) => ({
-    root: {
-        margin: theme.spacing.unit,
-        minWidth: 120,
-        marginBottom: '20px',
-        width: '100%'
-    },
-    typography: {
-        fontSize: 16
+const hasValue = val => val || val === 0;
+const getValue = val => (hasValue(val) ? val : '');
+
+class Dropdown extends Component {
+    change(e) {
+        const { onChange, propertyName, type } = this.props;
+        const { value } = e.target;
+
+        let val = value;
+
+        if (type === 'number') {
+            val = hasValue(value) ? parseFloat(value) : null;
+        }
+        val = hasValue(value) ? value : '';
+
+        onChange(propertyName, val);
     }
-});
 
-const Dropdown = ({ classes, label, items = [], onChange, value }) => (
-    <FormControl className={`${classes.root} ${classes.typography}`}>
-        <InputLabel className={classes.typography}>{label}</InputLabel>
-        <Select
-            className={classes.typography}
-            value={value}
-            onChange={onChange}
-        >
-            {items && items.map(item => (
-                <MenuItem key={item} className={classes.typography} value={item}>{item}</MenuItem>
-            ))}
-        </Select>
-    </FormControl>
-);
+    render() {
+        const {
+            disabled,
+            label,
+            items = [],
+            value,
+            helpText,
+            fullWidth,
+            adornment,
+            type
+        } = this.props;
+        return (
+            <TextField
+                id="outlined-select-currency-native"
+                type={type}
+                select
+                adornment={adornment}
+                disabled={disabled}
+                fullWidth={fullWidth}
+                InputLabelProps={{ shrink: true }}
+                label={label}
+                value={getValue(value)}
+                onChange={e => this.change(e)}
+                SelectProps={{
+                    native: true
+                }}
+                helperText={helpText}
+                margin="normal"
+                variant="outlined"
+            >
+                {items.map(item => (
+                    <option key={item} value={item}>
+                        {item}
+                    </option>
+                ))}
+            </TextField>
+        );
+    }
+}
 
-export default withStyles(styles)(Dropdown);
+Dropdown.propTypes = {
+    adornment: PropTypes.string,
+    disabled: PropTypes.bool,
+    fullWidth: PropTypes.bool,
+    helpText: PropTypes.string,
+    label: PropTypes.string.isRequired,
+    items: PropTypes.arrayOf(PropTypes.string),
+    propertyName: PropTypes.string,
+    value: PropTypes.string,
+    type: PropTypes.string,
+    onChange: PropTypes.func.isRequired
+};
+
+Dropdown.defaultProps = {
+    adornment: '',
+    disabled: false,
+    fullWidth: false,
+    propertyName: '',
+    items: [],
+    helpText: '',
+    type: 'text',
+    value: ''
+};
+
+export default Dropdown;
