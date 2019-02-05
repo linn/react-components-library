92189edafa94ad76284f7795facd885fc2b4705c
Lewis Renfrew <LewisRe@linn.co.uk>
2019-02-05 16:33:12 +0000

    on off switch

---
+++ src/components/OnOffSwitch.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from '@material-ui/core/Switch';
+import PropTypes from 'prop-types';
+
+class OnOffSwitch extends React.Component {
+    change = () => {
+        const { onChange, propertyName, value } = this.props;
+        onChange(propertyName, !value);
+    };
+
+    render() {
+        const { value, label, disabled } = this.props;
+        return (
+            <FormControlLabel
+                control={
+                    <Switch
+                        checked={value}
+                        onChange={() => this.change()}
+                        value={value}
+                        color="primary"
+                        disabled={disabled}
+                    />
+                }
+                label={label}
+            />
+        );
+    }
+}
+
+OnOffSwitch.propTypes = {
+    value: PropTypes.bool,
+    disabled: PropTypes.bool,
+    label: PropTypes.string,
+    onChange: PropTypes.func.isRequired,
+    propertyName: PropTypes.string.isRequired
+};
+
+OnOffSwitch.defaultProps = {
+    value: false,
+    disabled: false,
+    label: ''
+};
+
+export default OnOffSwitch;
---
+++ src/stories/OnOffSwitch.stories.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { withKnobs } from '@storybook/addon-knobs/react';
+import { text, boolean } from '@storybook/addon-knobs';
+import { action } from '@storybook/addon-actions';
+
+import OnOffSwitch from '../components/OnOffSwitch';
+
+const actions = {
+    onChange: action('onChange')
+};
+
+storiesOf('OnOffSwitch', module)
+    .addDecorator(story => <div>{story()}</div>)
+    .addDecorator(withKnobs)
+    .add('default ', () => (
+        <OnOffSwitch
+            label={text('label', 'Label')}
+            value={boolean('value', false)}
+            disabled={boolean('disabled', false)}
+            {...actions}
+            propertyName=""
+        />
+    ));
