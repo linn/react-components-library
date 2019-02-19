e814e253cae6ecb41871416a31a1ff2ae05a5f4a
richard phillips <richard.phillips@linn.co.uk>
2019-02-19 16:03:42 +0000

    Tidy up ExportButton

--- src/stories/ErrorCard.stories.js
+++ src/stories/ErrorCard.stories.js
@@ -6,12 +6,6 @@ import { text } from '@storybook/addon-knobs';
 import ErrorCard from '../components/ErrorCard';
 
 storiesOf('ErrorCard', module)
-    .addDecorator(story => (
-        <div style={{ padding: '3rem', width: "100%" }}>
-            {story()}
-        </div>
-    ))
+    .addDecorator(story => <div style={{ padding: '3rem', width: '100%' }}>{story()}</div>)
     .addDecorator(withKnobs)
-    .add('default ', () => (
-        <ErrorCard errorMessage={text('errorMessage', 'Error message')} />
-    ));
+    .add('default ', () => <ErrorCard errorMessage={text('errorMessage', 'Error message')} />);
