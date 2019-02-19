9c5137c5fba324ffde327644e23f5337aeb01c3a
Richard Phillips <richardiphillips@users.noreply.github.com>
2019-02-19 16:27:02 +0000

    Tidy up ExportButton (#23)

--- src/components/ExportButton.js
+++ src/components/ExportButton.js
@@ -1,29 +1,27 @@
-import React, { Component } from 'react';
-import { withStyles } from '@material-ui/core/styles';
+import React from 'react';
 import Button from '@material-ui/core/Button';
 import Tooltip from '@material-ui/core/Tooltip';
 import SvgIcon from '@material-ui/core/SvgIcon';
-const styles = theme => ({
+import PropTypes from 'prop-types';
 
-});
+const ExportButton = ({ href }) => (
+    <div style={{ float: 'right' }}>
+        <Tooltip title="Download report as CSV file" placement="top-end">
+            <Button href={href} color="primary" variant="outlined">
+                Export
+                <SvgIcon>
+                    <path
+                        xmlns="http://www.w3.org/2000/svg"
+                        d="M19 12v7H5v-7H3v7c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2v-7h-2zm-6 .67l2.59-2.58L17 11.5l-5 5-5-5 1.41-1.41L11 12.67V3h2z"
+                    />
+                </SvgIcon>
+            </Button>
+        </Tooltip>
+    </div>
+);
 
-class ExportButton extends Component {
-    render() {
-        const { classes, href } = this.props;
+ExportButton.propTypes = {
+    href: PropTypes.string.isRequired
+};
 
-        return (
-            <div className={classes.pullLeft}>
-             <Tooltip title="Download report as CSV file" placement="top-end">
-                <Button variant="contained" href={href}>
-                    Export  
-                    <SvgIcon>
-                      <path xmlns="http://www.w3.org/2000/svg" d="M19 12v7H5v-7H3v7c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2v-7h-2zm-6 .67l2.59-2.58L17 11.5l-5 5-5-5 1.41-1.41L11 12.67V3h2z"/>
-                    </SvgIcon>
-                </Button>
-              </Tooltip>
-            </div>
-        );
-    }
-}
-
-export default withStyles(styles)(ExportButton);
+export default ExportButton;
--- src/stories/ExportButton.stories.js
+++ src/stories/ExportButton.stories.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import  ExportButton  from '../components/ExportButton';
+import { withKnobs } from '@storybook/addon-knobs/react';
+import { text } from '@storybook/addon-knobs';
 
-export const actions = {
-  onClick: action('Back'),
-
-};
+import ExportButton from '../components/ExportButton';
 
 storiesOf('ExportButton', module)
-  .addDecorator(story => <div>{story()}</div>)
-  .add('default', () => <ExportButton {...actions} href="#" />);
+    .addDecorator(story => <div style={{ position: 'absolute', left: '50%' }}>{story()}</div>)
+    .addDecorator(withKnobs)
+    .add('default', () => <ExportButton text={text('href', '/href/')} />);
