653816ed1ca63223028ace0fb908e8f1889cddd9
richard phillips <richard.phillips@linn.co.uk>
2019-05-15 16:18:58 +0100

    use existing search field component.

--- src/components/TypeaheadDialog.js
+++ src/components/TypeaheadDialog.js
@@ -1,18 +1,11 @@
 import React, { Fragment, useState } from 'react';
-import {
-    List,
-    ListItem,
-    InputAdornment,
-    TextField,
-    Typography,
-    IconButton,
-    Dialog
-} from '@material-ui/core';
+import { List, ListItem, Typography, IconButton, Dialog } from '@material-ui/core';
 import PropTypes from 'prop-types';
 import SearchIcon from '@material-ui/icons/Search';
 import CloseIcon from '@material-ui/icons/Close';
 import useSearch from '../hooks/useSearch';
 import Loading from './Loading';
+import SearchInputField from './SearchInputField';
 
 const styles = {
     pullRight: {
@@ -39,6 +32,10 @@ const TypeaheadDialog = ({ title, loading, fetchItems, searchItems, onSelect, cl
 
     const handleClose = () => {
         setDialogOpen(false);
+    };
+
+    const handleSearchTermChange = (...args) => {
+        setSearchTerm(args[1]);
     };
 
     const showResults = () => {
@@ -82,26 +79,11 @@ const TypeaheadDialog = ({ title, loading, fetchItems, searchItems, onSelect, cl
                         <Typography variant="h5" gutterBottom>
                             {title}
                         </Typography>
-                        <TextField
+                        <SearchInputField
                             placeholder="Search by id or description"
-                            onChange={e => setSearchTerm(e.target.value)}
-                            type="search"
-                            margin="normal"
-                            variant="outlined"
-                            InputProps={{
-                                startAdornment: (
-                                    <InputAdornment position="start">
-                                        <svg
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            width="24"
-                                            height="24"
-                                            viewBox="0 0 24 24"
-                                        >
-                                            <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z" />
-                                        </svg>
-                                    </InputAdornment>
-                                )
-                            }}
+                            onChange={handleSearchTermChange}
+                            label=""
+                            value={searchTerm}
                         />
                         {loading ? <Loading /> : showResults()}
                     </div>
