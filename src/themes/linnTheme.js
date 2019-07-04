5d0f72e33a911f85b76688860abfe9716a32df4a
Adam Campbell <Adam.Campbell@linn.co.uk>
2019-07-04 17:35:23 +0100

    update main text colour

--- src/themes/linnTheme.js
+++ src/themes/linnTheme.js
@@ -1,10 +1,29 @@
 import { createMuiTheme } from '@material-ui/core/styles';
-import cyan from '@material-ui/core/colors/cyan';
+// import cyan from '@material-ui/core/colors/cyan';
 import pink from '@material-ui/core/colors/pink';
 
 export const linnTheme = createMuiTheme({
     palette: {
-        primary: cyan,
+        primary: {
+            50: '#e0f7fa',
+            100: '#b2ebf2',
+            200: '#80deea',
+            300: '#4dd0e1',
+            400: '#26c6da',
+            500: '#00bcd4',
+            600: '#00acc1',
+            700: '#0097a7',
+            800: '#00838f',
+            900: '#006064',
+            A100: '#84ffff',
+            A200: '#18ffff',
+            A400: '#00e5ff',
+            A700: '#00b8d4',
+            contrastText: 'rgba(0, 0, 0, 0.87)',
+            dark: '#0097a7',
+            light: '#4dd0e1',
+            main: '#00acc1'
+        },
         secondary: pink
     }
 });
