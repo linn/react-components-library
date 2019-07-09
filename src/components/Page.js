a009e735ce7c7a13e4e2ade6982a6cfde7941432
Adam Campbell <Adam.Campbell@linn.co.uk>
2019-07-09 11:51:01 +0100

    page to use styles

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,11 +1,11 @@
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-import withStyles from '@material-ui/styles/withStyles';
+import makeStyles from '@material-ui/styles/makeStyles';
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import Breadcrumbs from './Breadcrumbs';
 
-const styles = theme => ({
+const useStyles = makeStyles(theme => ({
     root: {
         padding: theme.spacing(4)
     },
@@ -18,27 +18,31 @@ const styles = theme => ({
     grid: {
         marginTop: theme.spacing(4)
     }
-});
+}));
 
-const Page = ({ classes, children, history }) => (
-    <Fragment>
-        <Grid container spacing={3} className={classes.grid}>
-            <Grid item xs={1} />
-            <Grid item xs={10}>
-                <Breadcrumbs history={history} />
+function Page({ children, history }) {
+    const classes = useStyles();
+
+    return (
+        <Fragment>
+            <Grid container spacing={3} className={classes.grid}>
+                <Grid item xs={1} />
+                <Grid item xs={10}>
+                    <Breadcrumbs history={history} />
+                </Grid>
+                <Grid item xs={1} />
+                {/* TODO would be good to have the mid width be a variable */}
+                <Grid item xs={1} />
+                <Grid item xs={10}>
+                    <Paper className={classes.root} square>
+                        {children}
+                    </Paper>
+                </Grid>
+                <Grid item xs={1} />
             </Grid>
-            <Grid item xs={1} />
-            {/* TODO would be good to have the mid width be a variable */}
-            <Grid item xs={1} />
-            <Grid item xs={10}>
-                <Paper className={classes.root} square>
-                    {children}
-                </Paper>
-            </Grid>
-            <Grid item xs={1} />
-        </Grid>
-    </Fragment>
-);
+        </Fragment>
+    );
+}
 
 Page.propTypes = {
     classes: PropTypes.shape({}).isRequired,
@@ -46,4 +50,4 @@ Page.propTypes = {
     history: PropTypes.shape({}).isRequired
 };
 
-export default withStyles(styles)(Page);
+export default Page;
