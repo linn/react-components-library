27204339335917ae4195dd7e156d1a3c624c4b96
Adam Campbell <Adam.Campbell@linn.co.uk>
2019-08-05 10:12:10 +0100

    add list sorting functions to utilities

--- src/utilities/__tests__/utilities.js
+++ src/utilities/__tests__/utilities.js
@@ -1,4 +1,4 @@
-﻿import { getHref, getSelfHref } from '../index';
+﻿import { getHref, getSelfHref, sortEntityList, sortList } from '../index';
 
 describe('when getting href', () => {
     let item;
@@ -104,6 +104,152 @@ describe('when getting getSelfHref', () => {
         test('should not return href', () => {
             expectedResult = null;
             expect(getSelfHref(item, 'r3')).toEqual(expectedResult);
+        });
+    });
+});
+
+describe('when sorting an entity list', () => {
+    const entityList = [
+        {
+            name: 'zara',
+            age: 44.1,
+            created: '2000-06-05T15:00:10.0000000'
+        },
+        {
+            name: 'alfie',
+            age: 66,
+            created: '2010-06-05T15:00:10.0000000'
+        },
+        {
+            name: 'lars',
+            age: 44.7,
+            created: '2005-06-05T15:00:10.0000000'
+        }
+    ];
+
+    describe('when sorting on string field', () => {
+        it('should sort the list', () => {
+            const expected = [
+                {
+                    name: 'alfie',
+                    age: 66,
+                    created: '2010-06-05T15:00:10.0000000'
+                },
+                {
+                    name: 'lars',
+                    age: 44.7,
+                    created: '2005-06-05T15:00:10.0000000'
+                },
+                {
+                    name: 'zara',
+                    age: 44.1,
+                    created: '2000-06-05T15:00:10.0000000'
+                }
+            ];
+
+            expect(sortEntityList(entityList, 'name')).toEqual(expected);
+        });
+    });
+
+    describe('when sorting on number field', () => {
+        it('should sort the list', () => {
+            const expected = [
+                {
+                    name: 'zara',
+                    age: 44.1,
+                    created: '2000-06-05T15:00:10.0000000'
+                },
+                {
+                    name: 'lars',
+                    age: 44.7,
+                    created: '2005-06-05T15:00:10.0000000'
+                },
+                {
+                    name: 'alfie',
+                    age: 66,
+                    created: '2010-06-05T15:00:10.0000000'
+                }
+            ];
+
+            expect(sortEntityList(entityList, 'age')).toEqual(expected);
+        });
+    });
+
+    describe('when sorting on invalid field', () => {
+        it('should not sort entity list', () => {
+            const expected = [
+                {
+                    name: 'zara',
+                    age: 44.1,
+                    created: '2000-06-05T15:00:10.0000000'
+                },
+                {
+                    name: 'alfie',
+                    age: 66,
+                    created: '2010-06-05T15:00:10.0000000'
+                },
+                {
+                    name: 'lars',
+                    age: 44.7,
+                    created: '2005-06-05T15:00:10.0000000'
+                }
+            ];
+
+            expect(sortEntityList(entityList, 'height')).toEqual(expected);
+        });
+    });
+
+    describe('when sorting on iso string field', () => {
+        it('should sort the list', () => {
+            const expected = [
+                {
+                    name: 'zara',
+                    age: 44.1,
+                    created: '2000-06-05T15:00:10.0000000'
+                },
+                {
+                    name: 'lars',
+                    age: 44.7,
+                    created: '2005-06-05T15:00:10.0000000'
+                },
+                {
+                    name: 'alfie',
+                    age: 66,
+                    created: '2010-06-05T15:00:10.0000000'
+                }
+            ];
+
+            expect(sortEntityList(entityList, 'created')).toEqual(expected);
+        });
+    });
+});
+
+describe('when sorting a list', () => {
+    describe('when sorting', () => {
+        it('should sort the list', () => {
+            const list = ['zara', 'alfie', 'lars'];
+
+            const expected = ['alfie', 'lars', 'zara'];
+
+            expect(sortList(list)).toEqual(expected);
+        });
+    });
+
+    describe('when sorting on a iso string', () => {
+        it('should sort the list', () => {
+            const list = [
+                '2005-06-05T15:00:10.0000000',
+                '2010-06-05T15:00:10.0000000',
+                '2000-06-05T15:00:10.0000000'
+            ];
+
+            const expected = [
+                '2000-06-05T15:00:10.0000000',
+                '2005-06-05T15:00:10.0000000',
+                '2010-06-05T15:00:10.0000000'
+            ];
+
+            expect(sortList(list)).toEqual(expected);
         });
     });
 });
--- src/utilities/index.js
+++ src/utilities/index.js
@@ -9,3 +9,25 @@ export const getHref = (itemWithLinks, rel) => {
 };
 
 export const getSelfHref = itemWithLinks => getHref(itemWithLinks, 'self');
+
+export const sortList = list =>
+    list.slice().sort((a, b) => {
+        if (a < b) {
+            return -1;
+        }
+        if (a > b) {
+            return 1;
+        }
+        return 0;
+    });
+
+export const sortEntityList = (list, property) =>
+    list.slice().sort((a, b) => {
+        if (a[property] < b[property]) {
+            return -1;
+        }
+        if (a[property] > b[property]) {
+            return 1;
+        }
+        return 0;
+    });
