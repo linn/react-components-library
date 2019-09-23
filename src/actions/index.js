243fa30f8da4cc1b943b3b5eaca0d1911c957dc1
lewisrenfrew <Lewis.Renfrew@linn.co.uk>
2019-09-23 14:47:39 +0100

    Update index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,6 +5,6 @@ export const REQUEST_NEWS = 'REQUEST_NEWS';
 export const RECEIVE_NEWS = 'RECEIVE_NEWS';
 export const MARK_NOTIFICATION_SEEN = 'MARK_NOTIFICATION_SEEN';
 
-export const FETCH_ERROR = 'FETCH_ERROR'; // no longer used??
+export const FETCH_ERROR = 'FETCH_ERROR'; 
 
 export const CLEAR_ITEM_ERRORS = 'CLEAR_ITEM_ERRORS';
