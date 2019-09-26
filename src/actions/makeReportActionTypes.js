b792a6a50ba2a2e67da900406501a88117505a4e
lewisre <lewisre@PC1152>
2019-09-26 10:53:24 +0100

    remove _REPORT suffix

--- src/actions/makeReportActionTypes.js
+++ src/actions/makeReportActionTypes.js
@@ -1,9 +1,9 @@
 const makeReportActionTypes = entityType => {
     const types = {};
-    types[`REQUEST_${entityType}_REPORT`] = `REQUEST_${entityType}_REPORT`;
-    types[`RECEIVE_${entityType}_REPORT`] = `RECEIVE_${entityType}_REPORT`;
-    types[`FETCH_${entityType}_REPORT_ERROR`] = `FETCH_${entityType}_REPORT_ERROR`;
-    types[`CLEAR_${entityType}_REPORT_ERRORS`] = `CLEAR_${entityType}_REPORT_ERRORS`;
+    types[`REQUEST_${entityType}`] = `REQUEST_${entityType}`;
+    types[`RECEIVE_${entityType}`] = `RECEIVE_${entityType}`;
+    types[`FETCH_${entityType}_ERROR`] = `FETCH_${entityType}_ERROR`;
+    types[`CLEAR_${entityType}_ERRORS`] = `CLEAR_${entityType}_ERRORS`;
 
     return types;
 };
