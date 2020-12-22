cb6761d3132b891cb306e365dfcabcda2a51d8d0
Adam Campbell <AdamC@linn.co.uk>
2020-12-22 17:09:29 +0000

    tidy

--- src/stories/LinkButton.stories.js
+++ src/stories/LinkButton.stories.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import StoryRouter from 'storybook-react-router';
-import Grid from '@material-ui/core/Grid';
 import providers from './renderUtils/Providers';
 import LinkButton from '../components/LinkButton';
 
-// const stories = storiesOf('LinkButton', module);
-
-// stories.addDecorator(StoryRouter()).addDecorator(story => providers(story));
 export default {
     title: 'LinkButton',
     decorators: [StoryRouter(), story => providers(story)]
