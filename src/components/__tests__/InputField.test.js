32166f0c799ba1e2fa7044582b1903465b1eac4c
Lewis Renfrew <lewis.renfrew@linn.co.uk>
2021-09-03 15:13:17 +0100

    write test that catches the issue

--- src/components/__tests__/InputField.test.js
+++ src/components/__tests__/InputField.test.js
@@ -1,34 +1,26 @@
 import React from 'react';
-import { createMount } from '@material-ui/core/test-utils';
-import { MuiThemeProvider } from '@material-ui/core/styles';
-import { linnTheme } from '../../themes';
+import '@testing-library/jest-dom/extend-expect';
+import render from '../../test-utils';
+
 import InputField from '../InputField';
 
 describe('<InputField />', () => {
-    let wrapper;
     let props;
-    const getTextField = () => wrapper.find('WithStyles(ForwardRef(TextField))');
-    const mount = createMount();
 
     beforeEach(() => {
         props = {
-            label: 'label',
+            label: 'some label',
+            propertyName: 'thePropertyName',
             value: 'value',
             onChange: () => {}
         };
-
-        const ComponentWithTheme = () => (
-            <MuiThemeProvider theme={linnTheme}>
-                <InputField {...props} />
-            </MuiThemeProvider>
-        );
-
-        wrapper = mount(<ComponentWithTheme {...props} />);
     });
 
     describe('when rendering input field', () => {
         it('should render text field', () => {
-            expect(getTextField()).toHaveLength(1);
+            const { getByLabelText } = render(<InputField {...props} />);
+
+            expect(getByLabelText('some label')).toBeInTheDocument();
         });
     });
 });
