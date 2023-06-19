67b38d7db07ff60afde56a552cb6cf384c9acc30
lewisrenfrew <Lewis.Renfrew@linn.co.uk>
2023-06-19 15:46:21 +0100

    Next prev buttons (#299)
    * component
    
    * stories etc
    
    * changelog and version++
    
    * fix typo
    
    ---------
    
    Co-authored-by: Lewis Renfrew <LewisRe@linn.co.uk>

---
+++ src/hooks/usePreviousNextNavigation.js
@@ -0,0 +1,42 @@
+import { useLocation, useParams } from 'react-router';
+import queryString from 'query-string';
+
+export default function usePreviousNextNavigation(
+    urlBuilder,
+    searchResults,
+    navigateFunction,
+    idStyle = 'param',
+    idFieldName
+) {
+    const { search } = useLocation();
+    const parsed = queryString.parse(search);
+    const { id } = useParams();
+
+    let idField;
+
+    if (idStyle === 'query') {
+        idField = parsed?.[idFieldName];
+    } else {
+        idField = id;
+    }
+
+    const currentIndex = searchResults?.indexOf(idField);
+    const nextResult = searchResults?.[currentIndex + 1];
+    const prevResult = searchResults?.[currentIndex - 1];
+
+    if (!searchResults?.length || searchResults?.length === 1) {
+        return [null, null];
+    }
+    const goNext = nextResult
+        ? () => {
+              navigateFunction(`${urlBuilder(nextResult)}`, { searchResults });
+          }
+        : null;
+    const goPrev = prevResult
+        ? () => {
+              navigateFunction(`${urlBuilder(prevResult)}`, { searchResults });
+          }
+        : null;
+
+    return [goPrev, goNext, prevResult, nextResult];
+}
