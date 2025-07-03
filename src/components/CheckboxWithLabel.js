f9fb206ddff36d4d3066727e7796fd42562b481a
Lewis Renfrew <lewis.renfrew@linn.co.uk>
2025-07-03 13:38:43 +0100

    kill withStyles

--- src/components/CheckboxWithLabel.js
+++ src/components/CheckboxWithLabel.js
@@ -1,27 +1,23 @@
 import React from 'react';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
-import withStyles from '@mui/styles/withStyles';
-
-const styles = {
-    root: {
-        width: '100%',
-        fontSize: 14
-    }
-};
 
 const CheckboxWithLabel = ({
-    classes = {},
-    checked = false,
-    color = 'primary',
-    label = '',
-    onChange
+  checked = false,
+  color = 'primary',
+  label = '',
+  onChange,
+  sx = {}
 }) => (
-    <FormControlLabel
-        label={label}
-        classes={{ label: `${classes.root}` }}
-        control={<Checkbox checked={checked} onChange={onChange} color={color} />}
-    />
+  <FormControlLabel
+    label={label}
+    sx={{
+      width: '100%',
+      fontSize: 14,
+      ...sx
+    }}
+    control={<Checkbox checked={checked} onChange={onChange} color={color} />}
+  />
 );
 
-export default withStyles(styles)(CheckboxWithLabel);
+export default CheckboxWithLabel;
